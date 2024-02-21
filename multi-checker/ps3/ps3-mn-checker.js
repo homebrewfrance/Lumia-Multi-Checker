fff169f335af060aa60ec4bf8a804aff9f1656c2
Dhalian <67284307+Dhalian@users.noreply.github.com>
2024-02-21 19:31:09 +0100

    Update ps3-mn-checker.js
    Moved MN Checker to this repo

--- multi-checker/ps3/ps3-mn-checker.js
+++ multi-checker/ps3/ps3-mn-checker.js
@@ -1,1 +1,28 @@
+/* CONTRIBUTE TO THIS SCRIPT ON OUR GITHUB https://github.com/homebrewfrance/Lumia-Multi-Checker */
+/* LAST-EDITED : 21/02/2023 by Dhalian */
 
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const numberInputPS3 = document.querySelector('input[name="numberInputPS3"]').value;
+    const CECHSelector = document.querySelector('select[name="CECHSelector"]').value;
+    const LETTERSelector = document.querySelector('select[name="LETTERSelector"]').value;
+
+    if (numberInputPS3.length < 7) {
+        alert("Numéro de modèle invalide.");
+        return;
+    }
+
+    if ((CECHSelector === "CECH-20" || CECHSelector === "CECH-21" || CECHSelector === "CECH-25") && (LETTERSelector === "A" || LETTERSelector === "B" || LETTERSelector === "C" || LETTERSelector === "D")) {
+        alert("[Toutes versions]\nVotre console est compatible avec le CFW.");
+
+    } else if ((CECHSelector === "CECH-30" || CECHSelector === "CECH-40" || CECHSelector === "CECH-42" || CECHSelector === "CECH-43") && (LETTERSelector === "A" || LETTERSelector === "B" || LETTERSelector === "C" || LETTERSelector === "D")) {
+        alert("[Toutes versions]\nVotre console est incompatible avec le CFW. Préférez plutôt une installation du HEN (Hybrid Firmware)");
+
+    } else if (["CECHA", "CECHB", "CECHC", "CECHE", "CECHF", "CECHG", "CECHH", "CECHJ", "CECHK", "CECHL", "CECHM", "CECHP", "CECHQ"].includes(CECHSelector)) {
+        alert("[Toutes versions]\nVotre console est compatible avec le CFW.");
+
+    } else {
+        alert("Numéro de modèle invalide ou non pris en charge.");
+    }
+});
